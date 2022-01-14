8d793a583ded1919808a406b2209be6a23e98e2c
ishii-0607 <96577145+ishii-0607@users.noreply.github.com>
2022-01-14 07:03:02 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9b907ca7ecfe984fe402bcc74f875b3799c792ec25755ef217d1627eedf607b686c1d60f6facb5ec59dbc3827dd5a73898fc4f8c8b12b165deee279c735cca52'
